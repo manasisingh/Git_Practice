4086bcb52a5d65625dbfeb23fd5efbca69047fe3
Manasi Singh <extended@Manasis-MacBook-Pro.local>
2022-07-11 01:56:02 +0530

    added React project with Comp1 and Comp2

---
+++ react_git_practice/Components/Comp1.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Comp1(){
+    <div>
+        <p>I am Comp 1</p>
+    </div>
+}
+
+export default Comp1
---
+++ react_git_practice/Components/Comp2.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Comp2(){
+    <div>
+        <p>I am Comp 2</p>
+    </div>
+}
+
+export default Comp2
